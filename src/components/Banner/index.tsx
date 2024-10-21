39751e6ef4b78009331773932eaa2a6b99c54e3d
Leo Coutinho <leocoutinho194@gmail.com>
2024-10-21 18:30:50 -0300

    atualizando header e banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,7 +1,15 @@
-import { Image } from './styles'
+import { Image, Title, Title2 } from './styles'
 
 import bannerImg from '../../assets/images/banner.png'
 
-const Banner = () => <Image style={{ backgroundImage: `url(${bannerImg})` }} />
-
+const Banner = () => (
+  <Image style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <Title>Italiana</Title>
+      <div>
+        <Title2>La Dolce Vita Trattoria</Title2>
+      </div>
+    </div>
+  </Image>
+)
 export default Banner
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,9 +1,29 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
 
 export const Image = styled.div`
   width: 100%;
-  height: 560px;
+  height: 304px;
   display: block;
   background-repeat: no-repeat;
   background-size: cover;
+`
+
+export const Title = styled.p`
+  font-size: 32px;
+  color: ${cores.branca};
+  padding-top: 25px;
+  margin-bottom: 157px;
+  font-weight: 100;
+  line-height: 37px;
+  font-style: normal;
+`
+
+export const Title2 = styled.h2`
+  font-size: 32px;
+  color: ${cores.branca};
+  font-weight: 900;
+  line-height: 37px;
+  font-style: normal;
+  margin-bottom: 32px;
 `
